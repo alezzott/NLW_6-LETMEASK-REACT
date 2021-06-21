6d548865fa0311facd2539f7947e410a1fbaf0c2
alezzo <alexkid12@hotmail.com>
2021-06-21 14:03:03 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from './components/button'
+
+function App() {
+  return (
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+
+  );
+}
+
+export default App;
+
+
